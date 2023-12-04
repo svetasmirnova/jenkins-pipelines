7c976e3c042b984593877799c2c399d2a6b2f418
Sveta Smirnova <sveta.smirnova@percona.com>
2023-12-04 23:02:05 +0300

    Modules for Ubuntu

--- pt/pt.groovy
+++ pt/pt.groovy
@@ -41,6 +41,9 @@ setup_ubuntu_package_tests = { ->
     sh '''
         sudo apt-get update
         sudo apt-get install -y libnuma1
+        sudo apt-get install -y libslurp-perl
+        sudo apt-get install -y libjson-perl
+        sudo apt-get install -y libnetaddr-ip-perl
         sudo apt-get install -y libdbi-perl
         sudo apt-get install -y libdbd-mysql-perl
     '''
