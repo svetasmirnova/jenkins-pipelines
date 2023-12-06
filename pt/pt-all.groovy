2ebc8ae47786062003f539e41c11a0fc7002e882
Sveta Smirnova <sveta.smirnova@percona.com>
2023-12-06 04:39:15 +0300

    Updated pt/pt-all.groovy

--- pt/pt-all.groovy
+++ pt/pt-all.groovy
@@ -1,6 +1,6 @@
 void runNodeBuild(String node_to_test) {
     build(
-        job: 'pt-package-testing',
+        job: 'pt',
         parameters: [
             string(name: 'node_to_test', value: params.node_to_test),
             string(name: 'MYSQL_VERSION', value: params.MYSQL_VERSION),
