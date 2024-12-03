66df9158a55cb9ec7960956e2edb2109c486333e
Sveta Smirnova <sveta.smirnova@percona.com>
2024-12-03 14:38:44 +0300

    Typo fix, try 2

--- pt/pt-unit-tests-molecule.groovy
+++ pt/pt-unit-tests-molecule.groovy
@@ -449,4 +449,5 @@ def setup_oel_molecule() {
             python3 -m pip install --upgrade setuptools
             python3 -m pip install --upgrade setuptools-rust
             python3 -m pip install --upgrade PyYaml==5.3.1 molecule==3.3.0 testinfra pytest molecule-ec2==0.3 molecule[ansible] "ansible<10.0.0" "ansible-lint>=5.1.1,<6.0.0" boto3 boto
-}        """
+        """
+}
