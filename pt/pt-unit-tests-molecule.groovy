0c6d55f2bcc2e9628a1cf137cf9ae4ffed396e92
Sveta Smirnova <sveta.smirnova@percona.com>
2025-09-23 20:03:21 +0300

    Testing molecule

--- pt/pt-unit-tests-molecule.groovy
+++ pt/pt-unit-tests-molecule.groovy
@@ -100,7 +100,7 @@
         )
     }
     options {
-        withCredentials(moleculePdpxcJenkinsCreds())
+        withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'c42456e5-c28d-4962-b32c-b75d161bff27', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']])
     }
 
         stages {
